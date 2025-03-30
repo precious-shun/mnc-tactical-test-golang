c15c4ca90b395f403104320ebafa63688d6635c6
Sunan Alit <sunanalit@gmail.com>
2025-03-30 16:37:04 +0700

    [ADD] learning conditional and boolean

--- main.go
+++ main.go
@@ -5,32 +5,31 @@ import (
 )
 
 func main() {
+	age := 25
 
-	// for loop with a single condition
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("x is", x)
-	// 	x++
-	// }
+	// Comparison operators
+	fmt.Println(age < 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
 
-	// for loop with a post statement
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("i is", i)
-	// }
-
-	// for loop with a range statement
-	names := []string{"Alice", "Bob", "Charlie"}
-	// for i, name := range names {
-	// 	fmt.Println("Name", i, "is", name)
-	// }
-
-	// for loop with a range statement and a blank identifier
-	// for i, name := range names {
-	// 	fmt.Printf("Name %d is %s\n", i, name)
-	// }
-
-	// _ is a blank identifier in Go, used to ignore values
-	for _, name := range names {
-		fmt.Printf("Name is %s\n", name)
+	// if-else statement
+	if age < 50 {
+		fmt.Println("You are young!")
+	} else if age == 50 {
+		fmt.Println("You are 50 years old!")
+	} else {
+		fmt.Println("You are old!")
 	}
+
+	// skip the iteration at index 1
+	names := []string{"Alice", "Bob", "Charlie"}
+	for index, name := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+		fmt.Printf("The value pos %v is %v\n", index, name)
+	}
+
 }
