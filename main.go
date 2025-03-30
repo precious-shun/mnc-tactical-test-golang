640c4fe1c6bfd2dda46cee61e34f2a6d82109d31
Sunan Alit <sunanalit@gmail.com>
2025-03-30 11:47:07 +0700

    [ADD] learning prints

--- main.go
+++ main.go
@@ -3,38 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	name := "Shun"
+	age := 18
 
-	//strings
-	var nameOne string = "Shun"
-	var nameTwo = "Arita"
-	var nameThree string
+	// Print
+	fmt.Print("---------Print---------\n")
+	fmt.Print("Hello, \n")
+	fmt.Print("World!\n")
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// Println
+	fmt.Println("---------Println---------")
+	fmt.Println("Hello, Shun")
+	fmt.Println("Goodbye, Shun!")
+	fmt.Println("My age is", age, "years old. And my name is", name, ".")
 
-	nameOne = "Shun Arita"
-	nameThree = "Arita Shun"
+	// Printf (formatted print)
+	fmt.Println("---------Printf---------")
+	fmt.Printf("Hello my name is %v, and my age is %v\n", name, age)
+	fmt.Printf("Hello my name is %q, and my age is %q\n", name, age)
+	fmt.Printf("Hello my name is %T, and my age is %T\n", name, age)
+	fmt.Printf("My score is %f\n", 3.14159265358979323846)
 
-	fmt.Println(nameOne, nameThree)
-
-	nameFour := "Shun"
-
-	fmt.Println(nameFour)
-
-	//integers
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	//bits and memory
-	var numOne int8 = 25
-	var numTwo int8 = -128
-	var numThree uint8 = 255
-	fmt.Println(numOne, numTwo, numThree)
-
-	//floats
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 238417098751.238417098751
-	scoreThree := 238417098751.238417098751
+	// Sprintf (save formatted string)
+	fmt.Println("---------Sprintf---------")
+	var str = fmt.Sprintf("Hello my name is %v, and my age is %v", name, age)
+	fmt.Printf("The saved string is %q", str)
 }
