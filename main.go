98036fd5b6f5f5933478b46bc4f24cd320121830
Sunan Alit <sunanalit@gmail.com>
2025-03-30 16:26:15 +0700

    [ADD] for loop learning

--- main.go
+++ main.go
@@ -1,35 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Arrays
-	fmt.Println("------------Arrays------------")
-	var ages [3]int = [3]int{10, 20, 30}
-	var shortenedAges = [3]int{10, 20, 30}
 
-	fmt.Println(ages, len(ages), cap(ages))
-	fmt.Println(shortenedAges, len(shortenedAges), cap(shortenedAges))
+	// for loop with a single condition
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("x is", x)
+	// 	x++
+	// }
 
-	names := [4]string{"John", "Doe", "Jane", "Smith"}
-	fmt.Println(names, len(names), cap(names))
+	// for loop with a post statement
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("i is", i)
+	// }
 
-	// Slices
-	fmt.Println("------------Slices------------")
-	var scores = []int{100, 90, 80}
-	scores[2] = 25
-	scores = append(scores, 65)
+	// for loop with a range statement
+	names := []string{"Alice", "Bob", "Charlie"}
+	// for i, name := range names {
+	// 	fmt.Println("Name", i, "is", name)
+	// }
 
-	fmt.Println(scores, len(scores), cap(scores))
+	// for loop with a range statement and a blank identifier
+	// for i, name := range names {
+	// 	fmt.Printf("Name %d is %s\n", i, name)
+	// }
 
-	// Slice Range
-	fmt.Println("------------Slice Range------------")
-	rangeOne := names[1:3]
-	fmt.Println(rangeOne, len(rangeOne), cap(rangeOne))
-	rangeTwo := names[2:]
-	fmt.Println(rangeTwo, len(rangeTwo), cap(rangeTwo))
-	rangeThree := names[:3]
-	fmt.Println(rangeThree, len(rangeThree), cap(rangeThree))
-	rangeOne = append(rangeOne, "New")
-	fmt.Println(rangeOne, len(rangeOne), cap(rangeOne))
+	// _ is a blank identifier in Go, used to ignore values
+	for _, name := range names {
+		fmt.Printf("Name is %s\n", name)
+	}
 }
