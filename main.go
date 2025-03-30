0fc3449a55bb853d5095d0910dd0e716b0af4b0b
Sunan Alit <sunanalit@gmail.com>
2025-03-30 16:53:31 +0700

    [ADD] learning functions

--- main.go
+++ main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+// Function that takes a string and prints a greeting message
+func sayGreeting(name string) {
+	fmt.Println("Hello, " + name + "!")
+}
+
+// Function that takes a string and prints a goodbye message
+func sayGoodbye(name string) {
+	fmt.Println("Goodbye, " + name + "!")
+}
+
+// Function that takes a slice of strings and a function as arguments
+func cycle(n []string, f func(string)) {
+	// Iterate over the slice of names and call the function f for each name
+	for _, name := range n {
+		f(name)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
+
 func main() {
-	age := 25
+	sayGreeting("Alice")
+	sayGoodbye("Bob")
 
-	// Comparison operators
-	fmt.Println(age < 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 50)
-	fmt.Println(age != 50)
+	names := []string{"Vaan", "Penelo", "Balthier", "Fran", "Basch", "Ashe", "Vayne"}
+	cycle(names, sayGreeting)
+	cycle(names, sayGoodbye)
 
-	// if-else statement
-	if age < 50 {
-		fmt.Println("You are young!")
-	} else if age == 50 {
-		fmt.Println("You are 50 years old!")
-	} else {
-		fmt.Println("You are old!")
-	}
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
 
-	// skip the iteration at index 1
-	names := []string{"Alice", "Bob", "Charlie"}
-	for index, name := range names {
-		if index == 1 {
-			fmt.Println("Continuing at pos", index)
-			continue
-		}
-		fmt.Printf("The value pos %v is %v\n", index, name)
-	}
-
+	fmt.Println(a1, a2)
+	fmt.Printf("Circle 1 is %0.3f and Circle 2 is %0.3f\n", a1, a2)
 }
