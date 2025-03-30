836f598f08ab15bc44628c01a6257a30390f5a97
Sunan Alit <sunanalit@gmail.com>
2025-03-30 12:26:30 +0700

    [ADD] learning array and slices

--- main.go
+++ main.go
@@ -3,29 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	name := "Shun"
-	age := 18
+	// Arrays
+	fmt.Println("------------Arrays------------")
+	var ages [3]int = [3]int{10, 20, 30}
+	var shortenedAges = [3]int{10, 20, 30}
 
-	// Print
-	fmt.Print("---------Print---------\n")
-	fmt.Print("Hello, \n")
-	fmt.Print("World!\n")
+	fmt.Println(ages, len(ages), cap(ages))
+	fmt.Println(shortenedAges, len(shortenedAges), cap(shortenedAges))
 
-	// Println
-	fmt.Println("---------Println---------")
-	fmt.Println("Hello, Shun")
-	fmt.Println("Goodbye, Shun!")
-	fmt.Println("My age is", age, "years old. And my name is", name, ".")
+	names := [4]string{"John", "Doe", "Jane", "Smith"}
+	fmt.Println(names, len(names), cap(names))
 
-	// Printf (formatted print)
-	fmt.Println("---------Printf---------")
-	fmt.Printf("Hello my name is %v, and my age is %v\n", name, age)
-	fmt.Printf("Hello my name is %q, and my age is %q\n", name, age)
-	fmt.Printf("Hello my name is %T, and my age is %T\n", name, age)
-	fmt.Printf("My score is %f\n", 3.14159265358979323846)
+	// Slices
+	fmt.Println("------------Slices------------")
+	var scores = []int{100, 90, 80}
+	scores[2] = 25
+	scores = append(scores, 65)
 
-	// Sprintf (save formatted string)
-	fmt.Println("---------Sprintf---------")
-	var str = fmt.Sprintf("Hello my name is %v, and my age is %v", name, age)
-	fmt.Printf("The saved string is %q", str)
+	fmt.Println(scores, len(scores), cap(scores))
+
+	// Slice Range
+	fmt.Println("------------Slice Range------------")
+	rangeOne := names[1:3]
+	fmt.Println(rangeOne, len(rangeOne), cap(rangeOne))
+	rangeTwo := names[2:]
+	fmt.Println(rangeTwo, len(rangeTwo), cap(rangeTwo))
+	rangeThree := names[:3]
+	fmt.Println(rangeThree, len(rangeThree), cap(rangeThree))
+	rangeOne = append(rangeOne, "New")
+	fmt.Println(rangeOne, len(rangeOne), cap(rangeOne))
 }
