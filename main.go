afa54e9368e319b243f5e66c3851a771bf36cc0d
Sunan Alit <sunanalit@gmail.com>
2025-03-30 11:06:11 +0700

    [ADD] studying data types

--- main.go
+++ main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	//strings
+	var nameOne string = "Shun"
+	var nameTwo = "Arita"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "Shun Arita"
+	nameThree = "Arita Shun"
+
+	fmt.Println(nameOne, nameThree)
+
+	nameFour := "Shun"
+
+	fmt.Println(nameFour)
+
+	//integers
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits and memory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint8 = 255
+	fmt.Println(numOne, numTwo, numThree)
+
+	//floats
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 238417098751.238417098751
+	scoreThree := 238417098751.238417098751
 }
